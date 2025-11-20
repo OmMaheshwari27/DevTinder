c9daca2d25f0f948b5f1e5559d3355d05e969b99
OmMaheshwari27 <oom12345609@gmail.com>
2025-11-20 23:50:10 +0530

    updated cors

--- src/app.js
+++ src/app.js
@@ -35,8 +35,13 @@ app.use((req, res, next) => {
 
 app.use(cookieParser());
 app.use(cors({
-  origin:  ["http://localhost:5173", "https://dev-tinder-f-three.vercel.app"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://dev-tinder-f-three.vercel.app"
+  ],
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
 
